c3c1e876c8b8c69147cd357124171f03fc3857c8
Victor Parizio <victorparizio@hotmail.com>
2023-04-03 17:06:55 -0300

    feat (App, Contato) Renderização da Lista de Contao Foi adicionado um componente Contato para renderizar os contatos que serão adicionado na lista. Também foi adicionado o package uuid para controle de id dos elementos da lista.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
 import React, { useState } from 'react'
+import Contato from './componentes/Contato'
+import {v4 as uuid} from 'uuid'
 
 export default function App() {
 
-  const [contato, setContato] = useState({nome: '', telefone:''})
+  const [contato, setContato] = useState({ nome: '', telefone: '' })
   const [listaContato, setListaContato] = useState([])
 
-  function novoNome(event){
-    setContato({...contato, nome: event.target.value})
+  function novoNome(event) {
+    setContato({ ...contato, nome: event.target.value })
   }
 
-  function novoTelefone(event){
-    setContato({...contato, telefone: event.target.value})
+  function novoTelefone(event) {
+    setContato({ ...contato, telefone: event.target.value })
   }
 
-  function addContato(){
+  function addContato() {
     setListaContato([...listaContato, contato])
   }
- 
+
   return (
     <>
       <h1>Lista de Contatos</h1>
       <hr />
       <div>
         <label>Nome:</label><br />
-        <input type="text" onChange={novoNome} value = {contato.nome} />
+        <input type="text" onChange={novoNome} value={contato.nome} />
       </div>
+
       <div>
         <label>Telefone:</label><br />
-        <input type="tel" onChange={novoTelefone} value = {contato.telefone} />
+        <input type="tel" onChange={novoTelefone} value={contato.telefone} />
       </div>
+
       <button onClick={addContato}>Adicionar Contato</button>
+
+      {listaContato.map(contato => {
+        return <Contato key={uuid()} nome={contato.nome} telefone={contato.telefone} />
+      })}
     </>
   )
 }
